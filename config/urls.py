5b574dae0beb7b1e073b802bfedfb8c37b557c56
mohammad <m.baniasadi.d@gmail.com>
2023-01-17 22:13:22 +0330

    adding djoser urls config

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
+
+# swagger config
 schema_view = get_schema_view(
     openapi.Info(
         title="Blog api swagger ",
@@ -19,6 +21,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # djoser url config
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
+
+    # swagger url config
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc',cache_timeout=0), name='schema-redoc'),

bea793fd53f4c7332189106ec74b69b431a42965
mohammad <m.baniasadi.d@gmail.com>
2023-01-21 19:19:02 +0330

    create Post and Category in api views file

--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.filters import OrderingFilter, SearchFilter
+
+from .serializers import PostListSerializer, PostDietaleSerializer, CategorySerializer
+from blog.models import Post, Category, Comment
+from .paginations import PostPageNumberPagination
+
+
+class PostListViwe(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content']
+    ordering_fields = ['title', 'content']
+    pagination_class = PostPageNumberPagination
+
+
+class PostDetaleApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostDietaleSerializer
+    queryset = Post.objects.all()
+
+
+class CategoryListApiView(ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryDetaleApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
